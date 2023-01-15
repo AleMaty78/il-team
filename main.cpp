2c78a31fd3e62481df5e65356b0cb94e28799087
Fedecim <federico.cimatti93@gmail.com>
2023-01-16 00:36:05 +0100

    modifica2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
     int a = 100;
     int b = 300;
+    //altra modifica
+    printf("NUOVA MODIFCA");
     cout << "Hello, World!" << endl;
     printf("%d",(a+b));
     return 0;
