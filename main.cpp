04501e7efb27b717905f9b3bc6d93ab0757740eb
AleMaty78 <122553385+AleMaty78@users.noreply.github.com>
2023-01-16 01:09:20 +0100

    PROVA MODIFICA EDITOR  DEV ON LINE

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main() {
     int a = 100;
     int b = 300;
+    int c = 0;
+    int d = 0;
     //altra modifica
     printf("NUOVA MODIFCA");
     cout << "Hello, World!" << endl;
-    printf("%d",(a+b));
+    printf("%d",(a+b+c+d));
+    printf("MODIFICA 2");
     return 0;
 }
